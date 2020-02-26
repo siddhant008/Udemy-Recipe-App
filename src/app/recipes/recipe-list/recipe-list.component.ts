578389ec09b0452c5603e25011eff2dffb183a5b
Siddhant Chourasia <siddhantchaurasia1999@gmail.com>
2020-02-27 01:30:59 +0530

    Delete Recipe working

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,24 +1,37 @@
-import { Component, OnInit} from '@angular/core';
-import { Recipe } from '../recipe.model';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {Recipe} from '../recipe.model';
 import {RecipeService} from '../recipe.service';
 import {ActivatedRoute, Router} from '@angular/router';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[];
+  private subscription: Subscription;
 
   constructor(private recipeService: RecipeService,
               private route: ActivatedRoute,
-              private router: Router) { }
+              private router: Router) {
+  }
 
   ngOnInit() {
+    this.subscription = this.recipeService.updatedRecipe.subscribe(
+      (newRecipes) => {
+        this.recipes = newRecipes;
+      }
+    );
     this.recipes = this.recipeService.getRecipes();
   }
+
   onNewRecipe() {
     this.router.navigate(['new'], {relativeTo: this.route});
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,13 @@
-import { Injectable} from '@angular/core';
+import {Injectable} from '@angular/core';
 
 import {Recipe} from './recipe.model';
 import {Ingredient} from '../shared/ingredient.model';
 import {ShoppingListService} from '../shopping-list/shopping-list.service';
+import {Subject} from 'rxjs';
 
 @Injectable()
 export class RecipeService {
-
+  updatedRecipe = new Subject<Recipe[]>();
   private recipes: Recipe[] = [
     new Recipe('McDonalds',
       'Meat with French fries',
@@ -24,14 +25,23 @@ export class RecipeService {
       ])
   ];
 
-  constructor(private shoppingListService: ShoppingListService) {}
+  constructor(private shoppingListService: ShoppingListService) {
+  }
+
   getRecipes() {
     return this.recipes.slice();
   }
-  getRecipe( id: number) {
+
+  getRecipe(id: number) {
     return this.recipes.slice()[id];
   }
+
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
-  this.shoppingListService.addIngredients(ingredients);
+    this.shoppingListService.addIngredients(ingredients);
+  }
+
+  deleteRecipe(id: number) {
+    this.recipes.splice(id, 1);
+    this.updatedRecipe.next(this.recipes.slice());
   }
 }
